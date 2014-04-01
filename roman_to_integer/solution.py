ffd12dc78fa2fb9fb2a69f05ae57a982617e5f9e
Shichao An <shichao.an@nyu.edu>
2014-03-31 22:42:25 -0400

    Correct version

--- roman_to_integer/solution.py
+++ roman_to_integer/solution.py
@@ -10,39 +10,10 @@ class Solution:
             'D': 500,
             'M': 1000,
         }
-        prefixes = {
-            'I': ('V', 'X'),
-            'X': ('L', 'C'),
-            'C': ('D', 'M'),
-        }
-        # Stack for storing prefixes
-        stack = []
         res = 0
-        for c in s:
-            if c not in prefixes:
-                if stack:
-                    # Subtract prefixes in stack
-                    if c in prefixes[stack[0]]:
-                        while stack:
-                            res -= roman[stack.pop()]
-                    else:
-                        while stack:
-                            res += roman[stack.pop()]
-                res += roman[c]
+        for i, c in enumerate(s):
+            if i < len(s) - 1 and roman[c] < roman[s[i + 1]]:
+                res -= roman[c]
             else:
-                if stack:
-                    if stack[0] == c:
-                        stack.append(c)
-                    else:
-                        if c in prefixes[stack[0]]:
-                            while stack:
-                                res -= roman[stack.pop()]
-                        else:
-                            while stack:
-                                res += roman[stack.pop()]
-                        stack.append(c)
-                else:
-                    stack.append(c)
-        while stack:
-            res += roman[stack.pop()]
+                res += roman[c]
         return res

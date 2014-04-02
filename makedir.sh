70d7b4dbc24b4dae07449d7db18a5f986120f5a3
Shichao An <shichao.an@nyu.edu>
2014-04-01 22:31:39 -0400

    Improved script

--- makedir.sh
+++ makedir.sh
@@ -4,7 +4,7 @@ usage="./makedir.sh title [...]"
 [ "$#" -eq 0 ] && { echo "$usage"; exit 1; }
 
 scriptdir=$(dirname "$0")
-newdir="$scriptdir/$(echo "$@" | tr 'A-Z ' 'a-z_')"
+newdir="$scriptdir/$(echo "$@" | tr 'A-Z ' 'a-z_' | tr -d "'")"
 
 [ -d "$newdir" ] && { echo "This directory already exists"; exit 1; } 
 

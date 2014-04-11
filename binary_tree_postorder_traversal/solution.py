15d4ef82022e61d2d6286b2a6c1eb91856f89cfc
Shichao An <shichao.an@nyu.edu>
2014-04-11 17:44:46 -0400

    Fixed

--- binary_tree_postorder_traversal/solution.py
+++ binary_tree_postorder_traversal/solution.py
@@ -17,11 +17,11 @@ class Solution:
         stack1.append(root)
         while stack1:
             root = stack1.pop()
+            stack2.append(root.val)
             if root.left is not None:
                 stack1.append(root.left)
             if root.right is not None:
                 stack1.append(root.right)
-            stack2.append(root.val)
         while stack2:
             path.append(stack2.pop())
         return path

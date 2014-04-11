c30f21791d1658d70a030a2806fe7fdd992fd1c2
Shichao An <shichao.an@nyu.edu>
2014-04-11 18:31:28 -0400

    Added preorder

---
+++ binary_tree_preorder_traversal/solution.py
@@ -0,0 +1,24 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def preorderTraversal(self, root):
+        path = []
+        if root is None:
+            return path
+        stack = []
+        stack.append(root)
+        while stack:
+            root = stack.pop()
+            path.append(root.val)
+            if root.right is not None:
+                stack.append(root.right)
+            if root.left is not None:
+                stack.append(root.left)
+        return path

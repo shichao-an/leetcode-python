89a8874059a86c53a4f2d22ba83743ed8c7ef1fd
Shichao An <shichao.an@nyu.edu>
2014-04-11 17:28:45 -0400

    Added binary tree postorder

---
+++ binary_tree_postorder_traversal/solution.py
@@ -0,0 +1,27 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def postorderTraversal(self, root):
+        path = []
+        if root is None:
+            return path
+        stack1 = []
+        stack2 = []
+        stack1.append(root)
+        while stack1:
+            root = stack1.pop()
+            if root.left is not None:
+                stack1.append(root.left)
+            if root.right is not None:
+                stack1.append(root.right)
+            stack2.append(root.val)
+        while stack2:
+            path.append(stack2.pop())
+        return path

e058a77d6b65591d652ae8f0aa44ac51c7dbca18
Shichao An <shichao.an@nyu.edu>
2014-04-28 00:24:42 -0400

    Fixed solution

--- generate_parentheses/solution.py
+++ generate_parentheses/solution.py
@@ -2,34 +2,16 @@ class Solution:
     # @param an integer
     # @return a list of string
     def generateParenthesis(self, n):
-        l = r = n
-        one = ''
-        stack = []
         res = []
-        self.gp(stack, l, r, one, res)
+        cand = ''
+        self.gp(n, n, cand, res)
         return res
 
-    def gp(self, stack, l, r, one, res):
-        if l == 0 and r == 0:
-            res.append(one)
+    def gp(self, left, right, cand, res):
+        if left > right or left < 0:
+            return
+        elif left == 0 and right == 0:
+            res.append(cand)
         else:
-            if not stack:
-                if l > 0:
-                    stack.append('(')
-                    one += '('
-                    self.gp(stack, l - 1, r, one, res)
-                    stack.pop()
-                    one = one[:-1]
-            else:
-                if l > 0:
-                    one += '('
-                    stack.append('(')
-                    self.gp(stack, l - 1, r, one, res)
-                    stack.pop()
-                    one = one[:-1]
-                if r > 0:
-                    stack.pop()
-                    one += ')'
-                    self.gp(stack, l, r - 1, one, res)
-                    stack.append('(')
-                    one = one[:-1]
+            self.gp(left - 1, right, cand + '(', res)
+            self.gp(left, right - 1, cand + ')', res)

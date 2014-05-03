a70ca7347a45d906d8b25d729aab6645795af7a8
Shichao An <shichao.an@nyu.edu>
2014-05-03 03:40:46 -0400

    Added binary tree zigzag

---
+++ binary_tree_zigzag_level_order_traversal/solution.py
@@ -0,0 +1,37 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def zigzagLevelOrder(self, root):
+        if root is None:
+            return []
+        res = []
+        queue = []
+        rev = False  # Reverse direction
+        level = []
+        queue.append(root)
+        queue.append(None)
+        while queue:
+            root = queue.pop(0)
+            if root is None:
+                if queue:
+                    queue.append(None)
+                res.append(level)
+                level = []
+                rev = not rev  # Toggle direction
+            else:
+                if rev:
+                    level.insert(0, root.val)
+                else:
+                    level.append(root.val)
+                if root.left is not None:
+                    queue.append(root.left)
+                if root.right is not None:
+                    queue.append(root.right)
+        return res

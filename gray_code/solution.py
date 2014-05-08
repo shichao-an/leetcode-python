b7847411cec868eebb40632015bf6da64200690c
Shichao An <shichao.an@nyu.edu>
2014-05-08 00:25:51 -0400

    Added gray code

--- gray_code/solution.py
+++ gray_code/solution.py
@@ -1,22 +1,14 @@
 class Solution:
     # @return a list of integers
     def grayCode(self, n):
-        if n == 0:
-            return [0]
-        else:
-            res = []
-            res.append(0)
-            for c in self.grayCode(n - 1):
-                nc = 2 ** (n - 1) + c
-                res.append(nc)
-            r = self.grayCode(n - 1)
-            r.reverse()
-            for c in r[:-1]:
-                res.append(c)
-            return res
-
-
-s = Solution()
-print map(lambda x: bin(x)[2:].zfill(2), s.grayCode(2))
-print map(lambda x: bin(x)[2:].zfill(3), s.grayCode(3))
-print map(lambda x: bin(x)[2:].zfill(4), s.grayCode(4))
+        m = 1 << n
+        res = []
+        d = [(1 << (i + 1)) / 2 for i in range(n)]
+        for i in range(1, m + 1):
+            num = 0
+            for j, e in enumerate(d):
+                if e / (1 << (j + 1)) % 2 == 1:
+                    num += 1 << j
+                d[j] += 1
+            res.append(num)
+        return res

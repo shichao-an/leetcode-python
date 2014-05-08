51f952743beb203e5f2bcfc0337d15d7ef6e3e9f
Shichao An <shichao.an@nyu.edu>
2014-05-08 00:34:43 -0400

    Fixed

--- gray_code/solution.py
+++ gray_code/solution.py
@@ -4,7 +4,7 @@ class Solution:
         m = 1 << n
         res = []
         d = [(1 << (i + 1)) / 2 for i in range(n)]
-        for i in range(1, m + 1):
+        for i in range(m):
             num = 0
             for j, e in enumerate(d):
                 if e / (1 << (j + 1)) % 2 == 1:

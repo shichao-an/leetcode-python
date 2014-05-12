4c46debaa523ac7795f69b7292964debe83108ca
Shichao An <shichao.an@nyu.edu>
2014-05-12 00:49:16 -0400

    Modified jump game ii

--- jump_game_ii/solution.py
+++ jump_game_ii/solution.py
@@ -5,18 +5,15 @@ class Solution:
         n = len(A)
         if len(A) == 1:
             return 0
-        t = [0 for i in range(n)]
-        for i in range(n):
-            t[i] = A[i] + i
-        count = 0
-        min_i = n - 1  # Mininum index able to reach `cur`
-        cur = n - 1  # Current index to reach (in a loop)
-        while i > 0:
-            while i >= 0:
-                if t[i] >= cur:
-                    min_i = i
-                i -= 1
-            count += 1
-            cur = min_i
-            i = cur
-        return count
+        start = 1
+        end = A[0]  # `end` is A[start - 1]
+        res = 1  # At least one step if len(A) > 1
+        while end < n - 1:
+            res += 1
+            max_end = 0  # `end` for the next loop
+            for i in range(start, end + 1):
+                if i + A[i] > max_end:
+                    max_end = i + A[i]
+            start = end + 1
+            end = max_end
+        return res

6f78a356b579c4147e3b22a863e143f08b66647d
Shichao An <shichao.an@nyu.edu>
2014-05-17 00:24:59 -0400

    Added linked list cycle

---
+++ linked_list_cycle/solution.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False

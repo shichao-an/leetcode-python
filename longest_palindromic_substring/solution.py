e8decf6689160f0e94a04e40ff058c5f048cf6f2
Shichao An <shichao.an@nyu.edu>
2014-05-22 21:40:45 -0400

    Added Longest Palindromic Substring (TLE)

---
+++ longest_palindromic_substring/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    # @return a string
+    def longestPalindrome(self, s):
+        n = len(s)
+        t = [[False for i in range(n)] for j in range(n)]
+        start = 0
+        max_len = 1
+        for i in range(n):
+            t[i][i] = True
+        for i in range(n - 1):
+            j = i + 1
+            if s[i] == s[j]:
+                t[i][j] = True
+                start = i
+                max_len = 2
+        l = 3  # Length
+        for l in range(3, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
+                if s[i] == s[j] and t[i + 1][j - 1]:
+                    t[i][j] = True
+                    start = i
+                    max_len = l
+        return s[start:start + max_len]
+
+
+a = 'faabaakcbc'
+s = Solution()
+print s.longestPalindrome(a)

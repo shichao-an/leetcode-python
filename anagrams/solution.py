12970c922abfbcf0c529a970f497027fb4852a95
Shichao An <shichao.an@nyu.edu>
2014-05-24 12:07:20 -0400

    Added modified/improved solution of anagrams

--- anagrams/solution.py
+++ anagrams/solution.py
@@ -5,13 +5,49 @@ class Solution:
         d = {}
         res = []
         for i, s in enumerate(strs):
-            ss = ''.join(sorted(s))
-            if ss in d:
-                if d[ss] >= 0:
-                    i = d[ss]
-                    res.append(strs[i])  # Append first anagram
-                    d[ss] = -1  # Flag this anagram
-                res.append(s)
+            key = self.make_key(s)
+            # First occurence of an anagram
+            if key not in d:
+                d[key] = i
             else:
-                d[ss] = i  # Added the index of the first anagram
+                if d[key] >= 0:
+                    # Append the first string of this anagram
+                    first_index = d[key]
+                    res.append(strs[first_index])
+                    d[key] = -1
+                res.append(s)
         return res
+
+    def make_key(self, s):
+        """Generate character-frequency key based on s"""
+        d = {}
+        res = []
+        for c in s:
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+        # Iterate form 'a' to 'z'
+        # This make sure the character occurences is ordered
+        # and thus unique
+        for i in range(ord('a'), ord('z') + 1):
+            c = chr(i)
+            if c in d:
+                res.append(c)
+                res.append(str(d[c]))
+        return ''.join(res)
+
+
+s = Solution()
+a = [
+    "cab",
+    "pug",
+    "pei",
+    "nay",
+    "ron",
+    "rae",
+    "ems",
+    "ida",
+    "mes"
+]
+print s.anagrams(a)

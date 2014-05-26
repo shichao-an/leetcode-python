d4505a16840b6574a5bbdf4907fee9674600d686
Shichao An <shichao.an@nyu.edu>
2014-05-26 00:10:05 -0400

    Fixed

--- valid_palindrome/solution.py
+++ valid_palindrome/solution.py
@@ -7,14 +7,14 @@ class Solution:
         left = 0
         right = len(s) - 1
         while left < right:
-            if not s[left].isalnum():
-                left += 1
-            if not s[right].isalnum():
-                right -= 1
             if s[left].isalnum() and s[right].isalnum():
-                if s[left].lower() == s[right].lower():
-                    left += 1
-                    right -= 1
-                else:
+                if s[left].lower() != s[right].lower():
                     return False
+                left += 1
+                right -= 1
+            else:
+                if not s[left].isalnum():
+                    left += 1
+                if not s[right].isalnum():
+                    right -= 1
         return True

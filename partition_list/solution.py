43bf2cd016d7c290d29112e59ebbf13441613058
Shichao An <shichao.an@nyu.edu>
2014-06-03 01:30:25 -0400

    Fixed

--- partition_list/solution.py
+++ partition_list/solution.py
@@ -9,20 +9,33 @@ class Solution:
     # @param x, an integer
     # @return a ListNode
     def partition(self, head, x):
-        l = ListNode(0)
-        g = ListNode(0)
-        p = l
-        q = g
+        if head is None:
+            return head
+        large = None
+        large_end = None
+        res = None
+        res_end = None
         while head is not None:
-            if head.val < x:
-                p.next = head
-                p = p.next
+            next_node = head.next
+            if head.val >= x:
+                if large is None:
+                    large = head
+                    large_end = large
+                else:
+                    large_end.next = head
+                    large_end = large_end.next
             else:
-                q.next = head
-                q = q.next
-            head = head.next
-        q.next = None
-        p.next = None
-        g = g.next
-        p.next = g
-        return l.next
+                if res is None:
+                    res = head
+                    res_end = res
+                else:
+                    res_end.next = head
+                    res_end = res_end.next
+            head = next_node
+        if large_end is not None:
+            large_end.next = None
+        if res is not None:
+            res_end.next = large
+        else:
+            res = large
+        return res

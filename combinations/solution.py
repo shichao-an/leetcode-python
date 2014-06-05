3919b59e211620643991eb93df898d9aa3814156
Shichao An <shichao.an@nyu.edu>
2014-06-05 18:10:30 -0400

    Fixed solution

--- combinations/solution.py
+++ combinations/solution.py
@@ -1,20 +1,17 @@
 class Solution:
     # @return a list of lists of integers
     def combine(self, n, k):
-        A = range(1, n + 1)
-        return self._combine(A, k)
+        a = range(1, n + 1)
+        return self.combine_aux(a, k)
 
-    def _combine(self, A, k):
+    def combine_aux(self, a, k):
         if k == 0:
-            return [[]]
-        if not A:
             return [[]]
         else:
             res = []
-            for i, c in enumerate(A):
-                if len(A[i + 1:]) >= k - 1 and k >= 0:
-                    rest_comb = self._combine(A[i + 1:], k - 1)
-                    for comb in rest_comb:
-                        comb.insert(0, c)
-                    res += rest_comb
+            for i, e in enumerate(a):
+                rest_comb = self.combine_aux(a[i + 1:], k - 1)
+                for comb in rest_comb:
+                    comb.insert(0, e)
+                res += rest_comb
             return res

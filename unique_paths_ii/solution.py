cbf5abfd1d27593164f5233a4e2cd652db2b6c41
Shichao An <shichao.an@nyu.edu>
2014-06-08 01:15:46 -0400

    Fixed bottom-up DP solution for unique paths

--- unique_paths_ii/solution.py
+++ unique_paths_ii/solution.py
@@ -2,27 +2,21 @@ class Solution:
     # @param obstacleGrid, a list of lists of integers
     # @return an integer
     def uniquePathsWithObstacles(self, obstacleGrid):
-        n = len(obstacleGrid)
-        m = len(obstacleGrid[0])
+        grid = obstacleGrid
+        n = len(grid)
+        m = len(grid[0])
+        t = [[-1 for i in range(m)] for j in range(n)]
         for i in range(n):
             for j in range(m):
-                if i == 0:
-                    if obstacleGrid[i][j] == 0:
-                        if j == 0 or (j > 0 and obstacleGrid[i][j - 1] != 0):
-                            obstacleGrid[i][j] = 1
-                    else:
-                        obstacleGrid[i][j] = 0
-                elif j == 0:
-                    if obstacleGrid[i][j] == 0:
-                        # i must be greater that zero here
-                        if obstacleGrid[i - 1][j] != 0:
-                            obstacleGrid[i][j] = 1
-                    else:
-                        obstacleGrid[i][j] = 0
+                if grid[i][j] == 1:
+                    t[i][j] = 0
                 else:
-                    if obstacleGrid[i][j] == 0:
-                        obstacleGrid[i][j] = (obstacleGrid[i - 1][j]
-                                              + obstacleGrid[i][j - 1])
+                    if i == 0 and j == 0:
+                        t[i][j] = 1
+                    elif i == 0:
+                        t[i][j] = t[i][j - 1]
+                    elif j == 0:
+                        t[i][j] = t[i - 1][j]
                     else:
-                        obstacleGrid[i][j] = 0
-        return obstacleGrid[n - 1][m - 1]
+                        t[i][j] = t[i - 1][j] + t[i][j - 1]
+        return t[n - 1][m - 1]

5e176d0cd7e229c2c174df05f281719d9d3b9d2b
Shichao An <shichao.an@nyu.edu>
2014-06-07 23:03:59 -0400

    Added top-down DP solution

---
+++ minimum_path_sum/solution2.py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param grid, a list of lists of integers
+    # @return an integer
+    def minPathSum(self, grid):
+        n = len(grid)
+        m = len(grid[0])
+        t = [[-1 for i in range(m)] for j in range(n)]
+        return self.min_path_sum_aux(grid, m - 1, n - 1, t)
+
+    def min_path_sum_aux(self, grid, x, y, t):
+        if x == 0 and y == 0:
+            return grid[y][x]
+        elif t[y][x] != -1:
+            return t[y][x]
+        elif x == 0 and y > 0:
+            t[y][x] = grid[y][x] + self.min_path_sum_aux(grid, x, y - 1, t)
+            return t[y][x]
+        elif x > 0 and y == 0:
+            t[y][x] = grid[y][x] + self.min_path_sum_aux(grid, x - 1, y, t)
+            return t[y][x]
+        else:
+            a = self.min_path_sum_aux(grid, x - 1, y, t)
+            b = self.min_path_sum_aux(grid, x, y - 1, t)
+            t[y][x] = grid[y][x] + min(a, b)
+            return t[y][x]

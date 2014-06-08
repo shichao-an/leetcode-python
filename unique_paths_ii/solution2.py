bb48dc699d410b61aaa317121ab1b17b6d167bc6
Shichao An <shichao.an@nyu.edu>
2014-06-08 01:06:16 -0400

    Fixed

--- unique_paths_ii/solution2.py
+++ unique_paths_ii/solution2.py
@@ -10,25 +10,19 @@ class Solution:
     def unique_paths(self, grid, x, y, t):
         if x == 0 and y == 0:
             return 1 if grid[y][x] == 0 else 0
+        elif grid[y][x] == 1:
+            t[y][x] = 0
+            return t[y][x]
         elif t[y][x] != -1:
             return t[y][x]
         elif x > 0 and y == 0:
-            if grid[y][x] == 1:
-                t[y][x] = 0
-            else:
-                t[y][x] = self.unique_paths(grid, x - 1, y, t)
+            t[y][x] = self.unique_paths(grid, x - 1, y, t)
             return t[y][x]
         elif y > 0 and x == 0:
-            if grid[y][x] == 1:
-                t[y][x] = 0
-            else:
-                t[y][x] = self.unique_paths(grid, x, y - 1, t)
+            t[y][x] = self.unique_paths(grid, x, y - 1, t)
             return t[y][x]
         else:
-            if grid[y][x] == 1:
-                t[y][x] = 0
-            else:
-                a = self.unique_paths(grid, x - 1, y, t)
-                b = self.unique_paths(grid, x, y - 1, t)
-                t[y][x] = a + b
+            a = self.unique_paths(grid, x - 1, y, t)
+            b = self.unique_paths(grid, x, y - 1, t)
+            t[y][x] = a + b
             return t[y][x]

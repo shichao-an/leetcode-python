acf775fccc2126db0cd5d75193c355fa51d175e1
Shichao An <shichao.an@nyu.edu>
2014-06-16 19:12:24 -0400

    Added alt soluion (TLE)

--- pow(x,_n)/solution3.py
+++ pow(x,_n)/solution3.py
@@ -5,10 +5,9 @@ class Solution:
     def pow(self, x, n):
         if n == 0:
             return 1.0
+        elif n == 1:
+            return x
         elif n < 0:
             return 1.0 / self.pow(x, -n)
         else:
-            if n % 2 == 0:
-                return self.pow(x * x, n / 2)
-            else:
-                return self.pow(x * x, (n - 1) / 2) * x
+            return self.pow(x, n / 2) * self.pow(x, n - n / 2)

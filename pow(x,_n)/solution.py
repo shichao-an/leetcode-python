a2f0e304fb94a6acdd572b4b74018953a3981b64
Shichao An <shichao.an@nyu.edu>
2014-06-16 18:57:53 -0400

    Fixed solution

--- pow(x,_n)/solution.py
+++ pow(x,_n)/solution.py
@@ -3,7 +3,20 @@ class Solution:
     # @param n, a integer
     # @return a float
     def pow(self, x, n):
-        if n % 2 == 0:
-            return pow(x * x, n / 2)
+        if n == 0:
+            return 1.0
+        elif n < 0:
+            return 1.0 / self.pow(x, -n)
         else:
-            return pow(x * x, (n - 1) / 2) * x
+            if n % 2 == 0:
+                return self.pow(x * x, n / 2)
+            else:
+                return self.pow(x * x, (n - 1) / 2) * x
+
+
+s = Solution()
+print s.pow(3, 5)
+print s.pow(5, 2)
+print s.pow(4, 0)
+print s.pow(2, -1)
+print s.pow(3, -3)

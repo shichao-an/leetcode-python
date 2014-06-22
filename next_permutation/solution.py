58b2a17e0f31a82edde1a3f3ad979bdddd4c60f2
Shichao An <shichao.an@nyu.edu>
2014-06-22 19:36:11 -0400

    Fixed

--- next_permutation/solution.py
+++ next_permutation/solution.py
@@ -4,26 +4,31 @@ class Solution:
     def nextPermutation(self, num):
         n = len(num)
         k = -1
+        l = -1
         # Find the largest k such that num[k] < num[k + 1]
         for i in range(n - 1):
             if num[i] < num[i + 1]:
                 k = i
 
-        # Find the largest l such that num[k] < num[l]
-        l = -1
+        # Find the largest l such that num[k] < num[l] (if k exists)
         if k >= 0:
             for i in range(n):
                 if num[i] > num[k]:
-                    l = i  # l must exist if k >= 0
+                    l = i
+            # Swap num[l] and num[k]
+            num[l], num[k] = num[k], num[l]
 
-        # Swap num[k] and num[l]
-        num[k], num[l] = num[l], num[k]
-
-        # Reverse num[k + 1:n]
-        l = k + 1
-        r = n - 1
-        while l < r:
-            num[l], num[r] = num[r], num[l]
-            l += 1
-            r -= 1
+        # Reverse num[k + 1:]
+        left = k + 1
+        right = n - 1
+        while left < right:
+            num[left], num[right] = num[right], num[left]
+            left += 1
+            right -= 1
         return num
+
+
+s = Solution()
+print s.nextPermutation([2, 1, 3])
+print s.nextPermutation([1, 2, 3])
+print s.nextPermutation([3, 2, 1])

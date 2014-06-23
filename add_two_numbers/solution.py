7793a5005292aac5be72e8fcde8361c6a3150b27
Shichao An <shichao.an@nyu.edu>
2014-06-23 01:06:48 -0400

    Fixed code

--- add_two_numbers/solution.py
+++ add_two_numbers/solution.py
@@ -7,44 +7,38 @@
 class Solution:
     # @return a ListNode
     def addTwoNumbers(self, l1, l2):
-        if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
         carry = 0
-        p = l1
-        q = l2
-        h = r = ListNode(0)
-        while q is not None and p is not None:
-            if p.val + q.val + carry > 9:
-                r.next = ListNode(p.val + q.val + carry - 10)
-                carry = 1
+        res = None
+        res_end = None
+        while l1 is not None and l2 is not None:
+            temp = l1.val + l2.val + carry
+            digit = temp % 10
+            carry = temp / 10
+            if res is None:
+                res = ListNode(digit)
+                res_end = res
             else:
-                r.next = ListNode(p.val + q.val + carry)
-                carry = 0
-            p = p.next
-            q = q.next
-            r = r.next
-        while q is not None:
-            if q.val + carry > 9:
-                r.next = ListNode(0)#
-                carry = 1
+                res_end.next = ListNode(digit)
+                res_end = res_end.next
+            l1 = l1.next
+            l2 = l2.next
+        rem = None
+        if l1 is not None:
+            rem = l1
+        else:
+            rem = l2
+        while rem is not None:
+            temp = rem.val + carry
+            digit = temp % 10
+            carry = temp / 10
+            if res is None:
+                res = ListNode(digit)
+                res_end = res
             else:
-                r.next = ListNode(q.val + carry)
-                carry = 0
-            q = q.next
-            r = r.next
-        while p is not None:
-            if p.val + carry > 9:
-                r.next = ListNode(0)#
-                carry = 1
-            else:
-                r.next = ListNode(p.val + carry)
-                carry = 0
-            p = p.next
-            r = r.next
+                res_end.next = ListNode(digit)
+                res_end = res_end.next
+            rem = rem.next
         if carry == 1:
-            r.next = ListNode(1)
-        return h.next
-    
-
+            res_end.next = ListNode(1)
+            res_end = res_end.next
+        return res

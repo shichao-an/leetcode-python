138bfd1ab703f6dff4563b05e5c6f1850d2de39a
Shichao An <shichao.an@nyu.edu>
2014-06-22 20:35:09 -0400

    Refactored code

--- reverse_nodes_in_k-group/solution.py
+++ reverse_nodes_in_k-group/solution.py
@@ -9,44 +9,45 @@ class Solution:
     # @param k, an integer
     # @return a ListNode
     def reverseKGroup(self, head, k):
-        if k <= 0:
+        if head is None:
             return head
-        # Calculate length
-        n = 0
         h = head
-        while head is not None:
+        n = 0
+        while h is not None:
             n += 1
-            head = head.next
-        g = n / k  # Number of groups
-        m = g * k  # Largest index (1-base) that can form a group
-        if m == 0:
-            return h
+            h = h.next
+        num_groups = n / k
+        # if k is larger than n, return head
+        if num_groups == 0:
+            return head
+        # `last_group` is the 1-base index of the last group node
+        last_group = num_groups * k
+        res = None
+        res_end = None
+        temp = None
+        temp_end = None
         i = 1
-        p = h
-        res = None  # Head of result list
-        end = res  # End node of result list
-        t = None  # Head of temporary list
-        t_end = t  # End node of temporary list
-        while p is not None:
-            next_node = p.next
-            if i <= m:
-                if t is None:
-                    t_end = p
-                p.next = t
-                t = p
+        while head is not None:
+            next_node = head.next
+            if i <= last_group:
+                # Appended current node to `temp` list
+                if temp is None:
+                    temp_end = head
+                head.next = temp
+                temp = head
+                # Appended `temp` list to `res`
                 if i % k == 0:
-                    # Append temporary list to result list
-                    if end is None:
-                        res = t
-                        end = t_end
+                    if res is None:
+                        res = temp
+                        res_end = temp_end
                     else:
-                        end.next = t
-                        end = t_end
-                    # Reset temporary list
-                    t = None
+                        res_end.next = temp
+                        res_end = temp_end
+                    # Reset `temp`
+                    temp = None
             else:
-                end.next = p
+                res_end.next = head
                 break
             i += 1
-            p = next_node
+            head = next_node
         return res

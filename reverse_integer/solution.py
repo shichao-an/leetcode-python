416b8209879034c6807d20b97a549abbb658c5e3
Shichao An <shichao.an@nyu.edu>
2014-06-23 01:25:07 -0400

    Fixed solution

--- reverse_integer/solution.py
+++ reverse_integer/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     # @return an integer
     def reverse(self, x):
-        t = 0
-        if x > 0:
-            positive = True
-        else:
-            positive = False
+        pos = True
+        if x < 0:
+            pos = False
             x = -x
+        t = 0
         while x != 0:
-            t = 10 * t + x % 10
+            t = t * 10 + x % 10
             x /= 10
-        return t if positive else -t
+        if not pos:
+            return -t
+        return t

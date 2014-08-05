adcccbbdaf1463555b4c96119929a8d750e6d992
Shichao An <shichao.an@nyu.edu>
2014-08-05 03:04:09 -0400

    Adde LRU cache (AC)

---
+++ lru_cache/solution2.py
@@ -0,0 +1,86 @@
+class LRUCache:
+
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.times = List()
+        self.cache = {}
+        self.timestamp = 0
+
+    # @return an integer
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            self.times.touch(node)
+            return node.value
+        return -1
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self.times.touch(node)
+        else:
+            if self.times.size >= self.capacity:
+                tail_node = self.times.tail
+                self.times.remove(tail_node)
+                del self.cache[tail_node.key]
+            node = ListNode(key, value)
+            self.cache[key] = node
+            # Insert node with key to the head
+            self.times.insert(node)
+
+
+class ListNode(object):
+    """Doubly Linked List node"""
+    def __init__(self, key, value):
+        self.prev = None
+        self.next = None
+        self.key = key
+        self.value = value
+
+
+class List(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def insert(self, node):
+        """Insert node to the head"""
+        node.next = self.head
+        if self.head is not None:
+            self.head.prev = node
+        else:
+            self.tail = node
+        self.head = node
+        self.size += 1
+
+    def touch(self, node):
+        """Move node to the head"""
+        prev_node = node.prev
+        next_node = node.next
+        if prev_node is not None:
+            prev_node.next = next_node
+            if next_node is not None:
+                next_node.prev = prev_node
+            else:
+                self.tail = prev_node
+            node.prev = None
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+
+    def remove(self, node):
+        """Remove node"""
+        prev_node = node.prev
+        if prev_node is not None:
+            prev_node.next = None
+            self.tail = prev_node
+        else:
+            self.head = None
+            self.tail = None
+        self.size -= 1

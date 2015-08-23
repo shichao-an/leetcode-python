f0e3ff1d8b3de11af2db6658908398ead54551e5
Shichao An <shichao.an@nyu.edu>
2015-08-22 20:27:23 -0700

    Added alternative solution

--- implement_strstr/solution2.py
+++ implement_strstr/solution2.py
@@ -8,11 +8,9 @@ class Solution(object):
         n = len(haystack)
         m = len(needle)
         for i in range(n + 1 - m):
-            matched = True
             for k in range(m):
                 if haystack[i + k] != needle[k]:
-                    matched = False
                     break
-            if matched:
+            else:
                 return i
         return -1

64280c9ae4b2682acd691c4fa748d7e8be894062
Shichao An <shichao.an@nyu.edu>
2015-08-24 19:09:22 -0700

    Added kth largest element in an array

---
+++ kth_largest_element_in_an_array/solution.py
@@ -0,0 +1,31 @@
+"""
+Find the kth largest element in an unsorted array. Note that it is the kth
+largest element in the sorted order, not the kth distinct element.
+
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note:
+You may assume k is always valid, 1 <= k <= array's length
+"""
+import heapq
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        h = []
+        for e in nums:
+            heapq.heappush(h, (-e, e))
+        for i in range(k):
+            w, e = heapq.heappop(h)
+            if i == k - 1:
+                return e
+
+a1 = [3, 2, 1, 5, 6, 4]
+s = Solution()
+res = s.findKthLargest(a1, 2)
+print(res)

010930d2cf46e9fe21f2cbacdfdb0ee0ea5ceabf
Shichao An <shichao.an@nyu.edu>
2015-08-28 11:30:01 -0700

    Added problem subjects

--- binary_tree_level_order_traversal/solution.py
+++ binary_tree_level_order_traversal/solution.py
@@ -1,3 +1,21 @@
+"""
+Given a binary tree, return the level order traversal of its nodes' values.
+(ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+"""
 # Definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):
--- validate_binary_search_tree/solution.py
+++ validate_binary_search_tree/solution.py
@@ -1,3 +1,15 @@
+"""
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's
+key.
+The right subtree of a node contains only nodes with keys greater than the
+node's key.
+Both the left and right subtrees must also be binary search trees.
+"""
+
 # Definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):

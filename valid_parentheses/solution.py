1ce37a790a8d69ee0afa24e010b1d6dc870a4940
Shichao An <shichao.an@nyu.edu>
2015-09-08 21:22:55 -0700

    Added problem subjects

--- valid_parentheses/solution.py
+++ valid_parentheses/solution.py
@@ -1,6 +1,18 @@
-class Solution:
-    # @return a boolean
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid
+but "(]" and "([)]" are not.
+"""
+
+
+class Solution(object):
     def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         pars = {
             ')': '(',
             ']': '[',

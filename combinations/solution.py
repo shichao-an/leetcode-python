59a728e96bfc746ed08eb9c07d823397e06a10b1
Shichao An <shichao.an@nyu.edu>
2015-09-10 18:55:41 -0700

    Fixed

--- combinations/solution.py
+++ combinations/solution.py
@@ -1,6 +1,28 @@
-class Solution:
-    # @return a list of lists of integers
+"""
+Given two integers n and k, return all possible combinations of k numbers out
+of 1 ... n.
+
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+"""
+
+
+class Solution(object):
     def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
         a = range(1, n + 1)
         return self.combine_aux(a, k)
 
--- permutations/solution.py
+++ permutations/solution.py
@@ -1,13 +1,25 @@
-class Solution:
-    # @param num, a list of integer
-    # @return a list of lists of integers
-    def permute(self, num):
-        if not num:
+"""
+Given a collection of numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+
+"""
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
             return [[]]
         else:
             res = []
-            for i, e in enumerate(num):
-                rest = num[:i] + num[i + 1:]
+            for i, e in enumerate(nums):
+                rest = nums[:i] + nums[i + 1:]
                 rest_perms = self.permute(rest)
                 for perm in rest_perms:
                     perm.append(e)
